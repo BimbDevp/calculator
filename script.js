b7b82e1a62de463c3dbe819af051e41ce17e4c3b
BimbDevp <BimbDevp@users.noreply.github.com>
2026-05-27 09:56:15 +0700

    feat: setup initial js variable and test first button click

---
+++ script.js
@@ -0,0 +1,22 @@
+// MEMORI INTERNAL
+const numberOne = "";
+const numberTwo = "";
+const operatorChoice = "";
+const resetStatus = "";
+
+// DOM Selectors
+const returnDisplay = document.querySelector(".big-number");
+const historyDisplay = document.querySelector(".small-number");
+const number = document.querySelector(".number");
+const operator = document.querySelector(".operator");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+
+// Math Logic Engine
+
+// UI Update Function
+
+// Event Listener
+number.addEventListener("click", () => {
+    console.log(number.textContent);
+});
